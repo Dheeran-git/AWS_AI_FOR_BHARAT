94b2035740307a740b995f662f897ed6c27ef02a
Dheeran-git <dheeran.karna@gmail.com>
2026-02-26 18:18:42 +0530

    Delete sarathi directory

--- sarathi/src/mocks/browser.ts
+++
@@ -1,5 +0,0 @@
-import { setupWorker } from 'msw/browser'
-import { citizenHandlers } from './handlers/citizens'
-import { panchayatHandlers } from './handlers/panchayat'
-
-export const worker = setupWorker(...citizenHandlers, ...panchayatHandlers)
